9bdfba30f1632db0617dc1cc0c1dbdb2556c65cd
WaifuForHire <thewritinginthesky@gmail.com>
2019-09-23 17:21:46 -0700

    fixed missing return

--- src/include/systemmanager.hpp
+++ src/include/systemmanager.hpp
@@ -37,7 +37,8 @@ namespace WaifuEngine
         template<typename _SysType>
         base_system * add_system()
         {
-            systems_.emplace(std::make_pair(_SysType::NAME, new _SysType()));
+            systems_.emplace_or_assign(std::make_pair(_SysType::NAME, new _SysType()));
+            return systems_[_SysType::NAME];
         }
 
         template<typename _SysType>
