a9895979e83423b66e8bdee0ea9733f132939106
Ryan <thewritinginthesky@gmail.com>
2019-09-26 12:22:53 -0700

    more style guide work

---
+++ docs/style_guide/src/fnccmt.cpp
@@ -0,0 +1,14 @@
+/**************************************************************************/
+      /*!
+        \param val
+			The value to be checked.
+        \return
+			Returns if given value is even or not.
+        \brief 
+			Checks that a given value is even.
+      */
+/**************************************************************************/
+bool is_even(int val)
+{
+	return val % 2 == 0;
+}
---
+++ docs/style_guide/src/hdrcmt.cpp
@@ -0,0 +1,13 @@
+/******************************************************************************/
+/*!
+\file   hdrcmt.cpp
+\author Ryan Hanson
+\par    email: iovita\@musicboxgames.net
+\par    Project: Full Hearts
+\date   9/25/2019
+\brief
+	This file is an example of a file header comment.
+
+\copyright    All content (c) 2019 Music Box Games, all rights reserved.
+*/
+/******************************************************************************/
