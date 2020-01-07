81011dbbf5948837c6b7bb4e7efe8c854162901b
WaifuForHire <thewritinginthesky@gmail.com>
2020-01-06 23:12:38 -0800

    [skip ci] bruh.png

--- src/include/event_manager.hpp
+++ src/include/event_manager.hpp
@@ -63,7 +63,7 @@ namespace waifuengine
         {
           if(events_[ename].count(instance))
           {
-            delete events_[ename][instance];
+            delete events_[ename][instance].first;
             events_[ename].erase(instance);
           }
         }
